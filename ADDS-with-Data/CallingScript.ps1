f3de311c64865c790a88542a79514bca664d6582
David Smith <dsmith@10thmagnitude.com>
2019-05-02 11:25:38 -0500

    CallingScript updates

--- ADDS-with-Data/CallingScript.ps1
+++ ADDS-with-Data/CallingScript.ps1
@@ -4,6 +4,7 @@
 # @GoateePFE: Shout out to @brwilkinson for assistance with some of this.
 
 
+# If you are not using the Azure Cloud Shell, follow the steps above to install the AZ module and login to your subscription
 # Install the Azure AZ Powershell modules from PowerShell Gallery
 # Takes a while to install 28 modules
 # Follow the instructions at the URL below if you do not already have the module installed on your workstation
@@ -16,32 +17,35 @@ Login-AzAccount
 Set-AzContext -SubscriptionName "MyMSDNSubscription"
 Set-AzContext -SubscriptionName "00000000-0000-0000-0000-000000000000"
 
-# Adjust the 'yourname' part below to something unique for you.
+# Adjust the 'yourname' part below to something unique for you and paste each line below into your shell
 # Set the location to the Azure Region you want to deploy resources to
 $URI       = 'https://raw.githubusercontent.com/cloudwidth/2019-Global-Azure-Bootcamp/master/ADDS-with-Data/azuredeploy.json'
 $Location  = 'South Central US'
-$namePrefix = 'yourname'                    ### cannot start with numbers, cannot be more than 11 characters - vmnaes will be vm+$NamePrefix+DC (i.e. vmMyVMNameDC)
+$namePrefix = 'dsmith'                    ### cannot start with numbers, cannot be more than 11 characters - vmnaes will be vm+$NamePrefix+DC (i.e. vmMyVMNameDC)
 $rgname    = ($namePrefix + '-rg')
 $addnsName = ($namePrefix).ToLower()            ### Used for the public DNS name of the VM being deployed.  
 
 
-# Check that the public dns $addnsName is available
-if (Test-AzDnsAvailability -DomainNameLabel $addnsName -Location $Location)
+# Check that the public dns $addnsName is available by pasting the line below into your shell
+if (Test-AzDnsAvailability -DomainNameLabel $addnsName -Location $Location) `
 { 'Available' } else { 'Taken. addnsName must be globally unique.' }
 
 
+# Paste the line below into your shell
 # Create the new resource group. Runs quickly.
 New-AzResourceGroup -Name $rgname -Location $Location
 
 # Parameters for the template and configuration
-$MyParams = @{
-    location              = $Location
-    domainName            = ($namePrefix + ".com")       ### The maximum length is 15 characters
-    addnsName             = $addnsName
-    namePrefix            = $namePrefix
+# Paste block of code below into your shell
+$MyParams = @{                                      
+    location              = $Location               
+    domainName            = ($namePrefix + ".com")        ### The maximum length is 15 characters
+    addnsName             = $addnsName              
+    namePrefix            = $namePrefix             
    }
 
 # Splat the parameters on New-AzureRmResourceGroupDeployment  
+# Paste block of code below into your shell
 $SplatParams = @{
     TemplateUri             = $URI 
     ResourceGroupName       = $rgname 
@@ -49,6 +53,7 @@ $SplatParams = @{
     Name                    = ($namePrefix + 'Forest')
    }
 
+# Paste the line below into your shell to deploy your newe domain controller
 # This takes ~30 minutes
 # One prompt for the domain admin password
 New-AzResourceGroupDeployment @SplatParams -Verbose
